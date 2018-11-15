f89a06d70b983b8428f573abab8992b1b53bd8ff
gsuppes <gsuppesx@hotmail.com>
2018-11-14 21:11:33 -0800

    Player prefab and movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,33 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+    Rigidbody2D myRigidBody;
+    [SerializeField] private float speed = 5f;
+
 	// Use this for initialization
 	void Start () {
+
+        myRigidBody = GetComponent <Rigidbody2D>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Run();
+        FlipSprite();
 	}
 
     private void Run()
     {
-        
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow*speed, myRigidBody.velocity.y); //keep y set to the current velocity
+        myRigidBody.velocity = playerVelocity; // update velocity with new x velocity
+    }
+
+    private void FlipSprite() { //reverse scaling of x-axis 
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon; // Mathf.Epsilon is a tiny floating point value
+        if (playerHasHorizontalSpeed) {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
